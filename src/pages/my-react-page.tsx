f4850dd3fd3c51f25bbe7ebbc163fd64a8f9470d
Chan Meng <Chan.Meng@lincolnuni.ac.nz>
2025-05-08 23:31:28 +1200

    Add Chinese translation feature

---
+++ src/pages/my-react-page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function MyReactPage(): React.ReactNode {
+  return (
+    <Layout>
+      <h1>My React page</h1>
+      <p>This is a React page</p>
+    </Layout>
+  );
+} 
