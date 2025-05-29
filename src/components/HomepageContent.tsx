136e243eb4f1febcfc8f047f49ca8345d3676b7c
Chan Meng <Chan.Meng@lincolnuni.ac.nz>
2025-05-30 00:16:33 +1200

    Delete ScrollingTestimonials section

--- src/components/HomepageContent.tsx
+++ src/components/HomepageContent.tsx
@@ -2,10 +2,7 @@ import React from 'react';
 import { Hero } from './Hero';
 import { CompetitionSection } from './CompetitionSection';
 import { Features } from './Features';
-import { ScrollingTestimonials } from './ScrollingTestimonials';
 import { LogoDivider } from './ui/LogoDivider';
-import { homepageConfig } from '../data';
-import { testimonialsData } from '../data/testimonials';
 
 export const HomepageContent: React.FC = () => {
   return (
@@ -15,11 +12,6 @@ export const HomepageContent: React.FC = () => {
       <CompetitionSection />
       <LogoDivider />
       <Features />
-      {/* <ScrollingTestimonials 
-        title={homepageConfig.testimonials.title}
-        subtitle={homepageConfig.testimonials.subtitle}
-        testimonials={testimonialsData}
-      /> */}
     </>
   );
 }; 
