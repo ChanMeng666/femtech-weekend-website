e883f5fa057b45cf109d19785efe7d94d99b8b43
ChanMeng666 <chanmeng.dev@gmail.com>
2025-07-13 15:41:56 +1200

    Add new advisor

--- src/components/AboutUs/AdvisorBoard.tsx
+++ src/components/AboutUs/AdvisorBoard.tsx
@@ -10,7 +10,7 @@ export function AdvisorBoard() {
     {
       name: "Celina Chew",
       role: "Former President of Bayer Group Greater China",
-      bio: "Celina is the former President of Bayer for Greater China.  She holds Bachelor and Master degrees in law. She worked as a private practice lawyer before moving in-house to the Bayer Greater China Legal Department in 1997.  She was head of Legal in Greater China (1998-2011), then became Country Group Head for Bayer North ASEAN Region (2011-2014) and President of Bayer, Greater China (2014-2019) overseeing Bayer’s operations in these regions. Celina has also been a Board member of various chambers of commerce, Enactus China and has acted as advisor to various organizations such as Ladies Who Tech, SilVR Adventures, Global Women Connect. She is honoured to be named in Forbes 100 Most Powerful Business Lists (2017 and 2018) and to receive an International Friendship Award from IESE in 2018, presented by the Queen of Spain. Celina is also featured in the book“China CEO II: Voices of Experience from 25 Top Executives Leading MNCs in China” by Juan Antonio Fernandez, Laurie Underwood.",
+      bio: "Celina is the former President of Bayer for Greater China.  She holds Bachelor and Master degrees in law. She worked as a private practice lawyer before moving in-house to the Bayer Greater China Legal Department in 1997.  She was head of Legal in Greater China (1998-2011), then became Country Group Head for Bayer North ASEAN Region (2011-2014) and President of Bayer, Greater China (2014-2019) overseeing Bayer's operations in these regions. Celina has also been a Board member of various chambers of commerce, Enactus China and has acted as advisor to various organizations such as Ladies Who Tech, SilVR Adventures, Global Women Connect. She is honoured to be named in Forbes 100 Most Powerful Business Lists (2017 and 2018) and to receive an International Friendship Award from IESE in 2018, presented by the Queen of Spain. Celina is also featured in the book \"China CEO II: Voices of Experience from 25 Top Executives Leading MNCs in China\" by Juan Antonio Fernandez, Laurie Underwood.",
       image: "/img/team/advisor-celina-chew.jpg",
     },
     {
@@ -24,6 +24,12 @@ export function AdvisorBoard() {
       role: "Healthcare Business Developer",
       bio: "Over 7+ years in startups scaling at-home diagnostics, SaMD, and remote patient monitoring platforms, Nicole has spearheaded GTM strategies for new product launches and built high-conversion, AI-automated sales funnels, cultivating a network of 500+ providers across pulmonology, mental health, and primary care. Combining operational execution with data-driven decision-making (MS, Healthcare Decision Analysis, USC), Nicole bridges the gap between innovation and clinical adoption—driving growth for cutting-edge healthtech solutions.",
       image: "/img/team/advisor-nicole-bu.svg",
+    },
+    {
+      name: "Huiqi (Yvonne) Lu",
+      role: "Lecturer in Engineering Science, University of Oxford",
+      bio: "Dr Huiqi Yvonne Lu is a researcher in AI for health and sensor informatics and a Lecturer in Engineering Science at the University of Oxford. Her research focuses on AI for digital health and sensor signal processing, and wearable devices for health monitoring (human, machine, and environmental changes). Dr Lu has led and co-led clinical AI and mobile health projects in academic and commercial settings and filed one patent. She has served as the conference and workshop committee member at notable conferences, including ICLR (PMLDC), NeurIPs (ML4H), IJCAI(KDHD), and the PHM. Dr Lu actively contributes to the IEEE Standard Committee for P3191: Performance Monitoring of Machine Learning-enabled Medical Devices in Clinical Use. She is an Associate Editor of Nature npj Women's Health, a Chief Editor of a special collection of \"Advances in AI for Women's Health, Reproductive Health, and Maternal Care: Bridging Innovation and Healthcare\". She also holds an honorary Research Fellow position at the George Institute for Global Health, Imperial College London.",
+      image: "/img/team/advisor-yvvone.jpeg",
     }
   ];
 
